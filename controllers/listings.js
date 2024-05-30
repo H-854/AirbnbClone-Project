dc9bd386924b0977bf741bcf0a0d148091c9fcdb
H854 <ranahritanjay@gmail.com>
2024-05-30 23:15:42 +0530

    updating image input option

--- controllers/listings.js
+++ controllers/listings.js
@@ -51,8 +51,11 @@ module.exports.renderNew = (req,res)=>{
 }
 
 module.exports.new = async (req, res) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url,filename};
     await newListing.save();
     req.flash("success","New Listing Created");
     res.redirect("/listings");
